a737a006fc83d8a4924ab6450d7702f2d6bdbc42
홍석범 <brent93.dev@gmail.com>
2024-08-28 16:48:34 +0900

    chore: Update login form to use password input widget

--- 06_WEB/0828/django_methods/account/views.py
+++ 06_WEB/0828/django_methods/account/views.py
@@ -10,7 +10,7 @@ def user_login(request): # 주소줄로 요청을 받아서 일할거야
         # 폼에서 데이터를 받아서 유효성 검사
         form = LoginForm(request.POST)
         if form.is_valid():
-            # 비우고
+            # form 형태를 dict 형태로 변환
             cd = form.cleaned_data 
             # 입력받은 username, password를 DB와 일치하는지 확인
             user = authenticate(request, username=cd['username'], password=cd['password'])
@@ -18,7 +18,7 @@ def user_login(request): # 주소줄로 요청을 받아서 일할거야
                 if user.is_active:
                     login(request, user)
 
-                    response = HttpResponse('인증 성공ㄴ') # 응답 객체 생성
+                    response = HttpResponse('인증 성공') # 응답 객체 생성
                     # 입력받은 user가 일치하면 response를 전달 로그인 되셨습니다!
                     return response
                 else:
